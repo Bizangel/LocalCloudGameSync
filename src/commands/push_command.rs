f2e8cfad9f5b0359b94d8e1799bf80b35115f183
Luis De Avila <bizangel10@gmail.com>
2025-10-03 00:08:50 +0100

    Implemented remote head parsing logic

--- src/commands/push_command.rs
+++ src/commands/push_command.rs
@@ -8,17 +8,30 @@ pub fn push_command(save_config_key: &String) -> Result<(), String> {
     // 1. Get remote lock
     let _lock = RemoteLock::acquire(&config.ssh_host)?;
 
-    // 2. Check remote head
-    let check_command = format!(
-        "cd {} && test -f .cloudmeta/{}",
-        &config.remote_save_folder_path, save_config_key
+    let exists_command = format!(
+        "cd {dir} 2>/dev/null || exit 100; \
+        [ -r .cloudmeta/{key}.HEAD ] && cat .cloudmeta/{key}.HEAD && exit 0; \
+        [ -e .cloudmeta/{key}.HEAD ] && exit 1; \
+        exit 2",
+        dir = &config.remote_save_folder_path,
+        key = &config.remote_backup_key
     );
 
-    println!("{}", check_command);
-    // let res = ssh_command(&global_save_options.ssh_host, &check_command)?;
-    let res = ssh_command(&config.ssh_host, &check_command)?;
+    let res = ssh_command(&config.ssh_host, &exists_command)?;
+    let head_output: Option<String> = match res.code.code() {
+        Some(0) => String::from_utf8(res.stdout)
+            .map(|x| Some(String::from(x.trim())))
+            .map_err(|e| format!("Unable to read file HEAD {}", e)),
+        Some(1) => Err(String::from("Remote HEAD file is not readable")),
+        Some(2) => Ok(None),
+        Some(_) | None => Err(format!(
+            "Error ocurred during checking SSH remote HEAD - Exit Code: \n{}",
+            String::from_utf8_lossy(&res.stderr)
+        )),
+    }?;
 
-    res.print();
+    println!("{:#?}", head_output);
+    // res.print();
 
     Ok(())
 }
