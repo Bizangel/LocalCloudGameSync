7b24bb7a814d9de8190d639b8ad54809cbad2478
Luis De Avila <bizangel10@gmail.com>
2025-10-15 23:27:04 +0100

    Vibe coded some style changes

--- src-ui/src/App.tsx
+++ src-ui/src/App.tsx
@@ -1,29 +1,28 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 import { useWebViewEvent } from './hooks/useGlobalRustEventListener'
 import { IPC, type WebViewUpdateEvent } from './ipc/common';
 
 function App() {
+  const [subtitle, setSubtitle] = useState("")
+  const [fadeKey, setFadeKey] = useState(0) // to trigger animation on text change
+
   const onWebViewChange = useCallback((ev: WebViewUpdateEvent) => {
     console.log("update: ", ev)
+    setSubtitle(ev.display_text)
+    setFadeKey(prev => prev + 1)
   }, []);
-  useWebViewEvent("WebViewUpdate", onWebViewChange);
 
-  const conflictresolve = useCallback(() => {
-    IPC.sendResolveConflict("push");
-  }, [])
+  useWebViewEvent("WebViewUpdate", onWebViewChange);
 
   return (
     <div className='container'>
-      <div className='centered-display'>
-
+      <div className='loading-wrapper'>
+        <div className='spinner'></div>
+        <h1>Loading</h1>
+        <p key={fadeKey} className='fade-text'>{subtitle}</p>
       </div>
-
-
-      <button onClick={conflictresolve}>
-        resolve with pull
-      </button>
-      </div>
+    </div>
   )
 }
 
